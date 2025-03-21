c8933fc8567dd65fde92501d302cbe790b91508c
Colcamex Resources Inc <dwarren@colcamex.com>
2025-03-20 20:54:32 -0700

    update reload file

--- reload.sh
+++ reload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is a safe, hot-reload script that will minimize downtime between reloads and check your config changes.
-#docker cp haproxy.cfg haproxy-proxy-1:/usr/local/etc/haproxy/haproxy_new.cfg
-#docker exec haproxy-proxy-1 cp /usr/local/etc/haproxy/haproxy_new.cfg /usr/local/etc/haproxy/haproxy.cfg
+docker compose cp haproxy.cfg proxy:/usr/local/etc/haproxy/haproxy_new.cfg
+docker compose exec proxy cp /usr/local/etc/haproxy/haproxy_new.cfg /usr/local/etc/haproxy/haproxy.cfg
 
 check=$(docker compose run proxy-checker -c -f /usr/local/etc/haproxy/haproxy.cfg | grep valid)
 
