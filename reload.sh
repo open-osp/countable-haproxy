890fda273eee210784449f408f4c32da2a0f5c0e
Colcamex Resources Inc <dwarren@colcamex.com>
2025-03-20 17:19:13 -0700

    upgrade docker compose in reload script

--- reload.sh
+++ reload.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # This is a safe, hot-reload script that will minimize downtime between reloads and check your config changes.
-check=$(docker-compose run proxy-checker -c -f /usr/local/etc/haproxy/haproxy.cfg | grep valid)
+#docker cp haproxy.cfg haproxy-proxy-1:/usr/local/etc/haproxy/haproxy_new.cfg
+#docker exec haproxy-proxy-1 cp /usr/local/etc/haproxy/haproxy_new.cfg /usr/local/etc/haproxy/haproxy.cfg
+
+check=$(docker compose run proxy-checker -c -f /usr/local/etc/haproxy/haproxy.cfg | grep valid)
 
 if [ -z "$check" ]
 then
       echo "Config check failed. Printing error information here:"
-      docker-compose run proxy-checker -c -f /usr/local/etc/haproxy/haproxy.cfg
+      docker compose run proxy-checker -c -f /usr/local/etc/haproxy/haproxy.cfg
 else
-      docker kill -s HUP haproxy_proxy_1
+      docker kill -s HUP haproxy-proxy-1
 fi
 
